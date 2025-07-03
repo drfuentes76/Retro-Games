f0a9b215aa7028789e60be00790cfd453ad2f7ae
drfuentes76 <drfuentes76@gmail.com>
2025-07-03 07:51:41 -0500

    Add files via upload

---
+++ matchmaking.js
@@ -0,0 +1,13 @@
+
+export function generateJoinCode(length = 6) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
+}
+
+export function redirectToRoom(code) {
+  window.location.href = `index.html?room=${code}`;
+}
